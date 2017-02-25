e3749f6fe0ec6b62d0c2a4d5d1ebde129d6e5bdf
lelinh <lemanhlinhbk@gmail.com>
2017-02-25 11:14:27 +0700

    first commit

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Linh Le on 2/22/17.
+//  Copyright © 2017 CoderSchool. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
